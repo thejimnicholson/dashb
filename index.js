02a88543e2717d151d2d428a4f874bbd4eb978b5
Jim Nicholson <thejimnicholson@gmail.com>
2023-01-26 18:11:16 -0800

    Rough go at setting background based on time

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 setInterval(displayTime, 1000);
 
+$(document).ready(function(){
+
+    var times = SunCalc.getTimes(new Date(), 45.51, -122.84);
+    document.getElementById('sunrise').innerText = "Sunrise: " + times.sunrise.getHours() + ':' + times.sunrise.getMinutes();
+    document.getElementById('sunset').innerText = "Sunset: " + times.sunset.getHours() + ':' + times.sunset.getMinutes();
+	var d = new Date();
+	var n = d.getHours();
+	if ((n > (times.sunset.getHours() + 1)) || (n < times.sunrise.getHours() -1))
+	  // If time is after 7PM or before 6AM, apply night theme to ‘body’
+	  document.body.className = "night";
+	else if (n >= (times.sunset.getHours() - 1) && n <= (times.sunset.getHours() + 1))
+	  // If time is between 4PM – 7PM sunset theme to ‘body’
+	  document.body.className = "sunset";
+	else
+	  // Else use ‘day’ theme
+	  document.body.className = "day";
+});
+
 function displayTime() {
 
     const timeNow = new Date();
