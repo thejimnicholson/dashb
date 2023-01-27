693981ee5eb83c4f81ce94d981b70512822f8498
Jim Nicholson <thejimnicholson@gmail.com>
2023-01-27 14:07:48 -0800

    More DRY, cleanup code

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+let myLat = 45.51;
+let myLong = -122.84;
+
+
 setInterval(displayTime, 1000);
 
+
 $(document).ready(function() {
-    var times = SunCalc.getTimes(new Date(), 45.51, -122.84);
+    var times = SunCalc.getTimes(new Date(), myLat, myLong);
     document.getElementById('sunrise').innerText = "Sunrise: " + formatTime(times.sunrise);
     document.getElementById('sunset').innerText = "Sunset: " + formatTime(times.sunset);
     document.body.className = getBackground(new Date(),times.sunrise,times.sunset);
@@ -14,29 +19,32 @@ function getBackground(thisTime,sunrise,sunset) {
         bg_class = "sunrise";
     else if ((n >= sunset.getHours() - 1) && n <= (sunset.getHours() + 1))
         bg_class = "sunset";
-	else if ((n >= (sunset.getHours() + 1)) || (n <= sunrise.getHours() -1))
+	else if ((n >= (sunset.getHours() + 1)) || (n <= sunrise.getHours() - 1))
         bg_class = "night";
     return bg_class;
 }
 
 
-function formatTime(thisTime) {
+function formatTime(thisTime, showSecs = false) {
     let hrs = thisTime.getHours();
     let mins = thisTime.getMinutes();
+    let secs = thisTime.getSeconds();
     let period = "AM";
     if (hrs > 12) {
         hrs -= 12;
         period = "PM";
     }
-
     if (hrs === 0) {
         hrs = 12;
         period = "AM"
     }
-
     hrs = hrs < 10 ? "0" + hrs : hrs;
     mins = mins < 10 ? "0" + mins : mins;
     let time = hrs + ":" + mins + period;
+    if (showSecs) {
+        secs = secs < 10 ? "0" + secs : secs;
+        time = hrs + ":" + mins + ":" + secs + period;
+    }
     return time;
 }
 
@@ -58,30 +66,13 @@ function displayTime() {
     let period = "AM";
 
     if (minutes % 5 === 0) {
-        var times = SunCalc.getTimes(new Date(), 45.51, -122.84);
-
-
+        var times = SunCalc.getTimes(timeNow, myLat, myLong);
         document.getElementById('sunrise').innerText = "Sunrise: " + formatTime(times.sunrise);
         document.getElementById('sunset').innerText = "Sunset: " + formatTime(times.sunset);
         document.body.className = getBackground(timeNow,times.sunrise,times,sunset);
     }
 
-    if (hoursOfDay > 12) {
-        hoursOfDay-= 12;
-        period = "PM";
-    }
-
-    if (hoursOfDay === 0) {
-        hoursOfDay = 12;
-        period = "AM";
-    }
-
-    hoursOfDay = hoursOfDay < 10 ? "0" + hoursOfDay : hoursOfDay;
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
-
-    let time = hoursOfDay + ":" + minutes + ":" + seconds + period;
-
+    let time = formatTime(timeNow,true);
     document.getElementById('Clock').innerHTML = time + "<br/>" + today + "<br/>" + months + " " + thisDay;
 
 }
